f635ff9cef8f83db9f02733a515a6e7c894d7575
CharlesO55 <charles_mathew_ong@dlsu.edu.ph>
2024-09-12 10:45:03 +0800

    01 Window

---
+++ DirectXTutorial/Window.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <Windows.h>
+
+class Window
+{
+public:
+	Window();
+	bool init();
+	bool broadcast();
+	bool release();
+	bool isRun();
+	~Window();
+
+
+	virtual void onCreate()=0;
+	virtual void onUpdate()=0;
+	virtual void onDestroy();
+
+protected:
+	HWND m_hwnd;
+	bool m_is_run;
+};
+
